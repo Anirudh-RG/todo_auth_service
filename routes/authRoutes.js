86b4d0a46af69213cd3457e3467382d059ba5b09
Anirudh-RG <i-Anirudh@roundglass.com>
2025-05-23 04:41:11 +0000

    today-route-added

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,4 +4,8 @@ const { register, login } = require('../controllers/authController');
 const router = express.Router();
 router.post('/register', register);
 router.post('/login', login);
+router.get('/today', (req, res) => {
+  const today = new Date();
+  res.status(200).json({ message: `Today is ${today.toDateString()}` });
+});
 module.exports = router;
